9bdd6a8d345e367c1917e270ac900759cf943d1c
ashoka <savebtc11@gmail.com>
2018-10-14 16:02:26 +0800

    Updates

---
+++ precache-manifest.521979e09fdebf00a6e3d21d0ae096c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "0e1103e7706d74eb3dd7",
+    "url": "/tic-tac-toe/static/js/main.0e1103e7.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "/tic-tac-toe/static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "0e1103e7706d74eb3dd7",
+    "url": "/tic-tac-toe/static/css/main.49672b9b.chunk.css"
+  },
+  {
+    "revision": "97f2176c7a2545d1883ff5d1b6fa06b4",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
